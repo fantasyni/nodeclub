76c35798ab2699021aa72ec07f359a94e8f98137
fantasyni <fantasyni@163.com>
2013-12-20 12:46:21 +0800

    fix xss data url

--- libs/util.js
+++ libs/util.js
@@ -80,6 +80,48 @@ exports.escape = function (html) {
   .replace(/\n\n$/, '');
 };
 
+var xssOptions = {
+  whiteList: {
+    h1:     [],
+    h2:     [],
+    h3:     [],
+    h4:     [],
+    h5:     [],
+    h6:     [],
+    hr:     [],
+    span:   [],
+    strong: [],
+    b:      [],
+    i:      [],
+    br:     [],
+    p:      [],
+    pre:    ['class'],
+    code:   [],
+    a:      ['target', 'href', 'title'],
+    img:    ['src', 'alt', 'title'],
+    div:    [],
+    table:  ['width', 'border'],
+    tr:     [],
+    td:     ['width', 'colspan'],
+    th:     ['width', 'colspan'],
+    tbody:  [],
+    thead:  [],
+    ul:     [],
+    li:     [],
+    ol:     [],
+    dl:     [],
+    dt:     [],
+    em:     [],
+    cite:   [],
+    section: [],
+    header: [],
+    footer: [],
+    blockquote: [],
+    audio:  ['autoplay', 'controls', 'loop', 'preload', 'src'],
+    video:  ['autoplay', 'controls', 'loop', 'preload', 'src', 'height', 'width']
+  }
+};
+
 /**
  * 过滤XSS攻击代码
  *
@@ -87,5 +129,7 @@ exports.escape = function (html) {
  * @return {string}
  */
 exports.xss = function (html) {
-  return xss(html);
+  return xss(html, xssOptions)
+    .replace(/<a\s*href="\s*data:.*?".*?>/, "")
+    .replace(/<img\s*src="\s*data:.*?".*?>/, "");
 };
